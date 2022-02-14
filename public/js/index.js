46c44c2903d27f5ab55e60156ccd64b6deef4aec
Bryth <62302815+Brythzz@users.noreply.github.com>
2022-02-14 14:04:48 +0100

    Add save button

--- public/js/index.js
+++ public/js/index.js
@@ -107,7 +107,8 @@ const showLogin = () => {
 };
 
 const showEditor = () => {
-    document.body.innerHTML = '<div id="lines">&gt;</div><textarea spellcheck="false" id="box"></textarea><div id="logout"><svg viewBox="0 0 20 20"><path d="M20,15a3,3,0,0,1-3,3H14a1,1,0,0,1,0-2h3a1,1,0,0,0,1-1V5a1,1,0,0,0-1-1H14a1,1,0,0,1,0-2h3a3,3,0,0,1,3,3Zm-6.293-4.293-5,5a1,1,0,0,1-1.414-1.414L10.586,11H1A1,1,0,0,1,1,9h9.586L7.293,5.707A1,1,0,1,1,8.708,4.293h0l5,5a1,1,0,0,1,0,1.414h0Z" fill="#fff"/></svg></div>';
+    document.body.innerHTML = '<div id="lines">&gt;</div><textarea spellcheck="false" id="box"></textarea><div class="menu"><div id="save"><svg viewBox="0 0 448 512"><path d="M433.941 129.941l-83.882-83.882A48 48 0 0 0 316.118 32H48C21.49 32 0 53.49 0 80v352c0 26.51 21.49 48 48 48h352c26.51 0 48-21.49 48-48V163.882a48 48 0 0 0-14.059-33.941zM224 416c-35.346 0-64-28.654-64-64s28.654-64 64-64 64 28.654 64 64-28.654 64-64 64zm96-304.52V212c0 6.627-5.373 12-12 12H76c-6.627 0-12-5.373-12-12V108c0-6.627 5.373-12 12-12h228.52c3.183 0 6.235 1.264 8.485 3.515l3.48 3.48A11.996 11.996 0 0 1 320 111.48z"/></svg></div><div id="logout"><svg viewBox="0 0 20 20"><path d="M20,15a3,3,0,0,1-3,3H14a1,1,0,0,1,0-2h3a1,1,0,0,0,1-1V5a1,1,0,0,0-1-1H14a1,1,0,0,1,0-2h3a3,3,0,0,1,3,3Zm-6.293-4.293-5,5a1,1,0,0,1-1.414-1.414L10.586,11H1A1,1,0,0,1,1,9h9.586L7.293,5.707A1,1,0,1,1,8.708,4.293h0l5,5a1,1,0,0,1,0,1.414h0Z" fill="#fff"/></svg></div><div>';
+    document.getElementById('save').addEventListener('click', saveHaste);
     document.getElementById('logout').addEventListener('click', logout);
 }
 
